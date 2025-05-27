ff97149a7613e64bbfd049e9ed848719863ae49c
ibrahim-pk <ibrahimpk220@gmail.com>
2025-05-27 13:00:24 +0600

    fix error in test file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',     // 👈 this simulates a browser
+    globals: true,            // allows using describe, it, expect without imports
+    setupFiles: './vitest.setup.js', // optional: for jest-dom
+  },
 })
