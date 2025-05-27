8994232234a805c1fb74d9b7154ee7381de0bc99
ibrahim-pk <ibrahimpk220@gmail.com>
2025-05-27 13:25:07 +0600

    test error check after adding needs

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,7 +6,7 @@ import { describe, it, expect } from 'vitest';
 describe('App Component', () => {
   it('renders an h1 with "Github workflow"', () => {
     render(<App />);
-    const heading = screen.getByRole('heading', { level: 1 });
+    const heading = screen.getByRole('heading', { level: 2 });
     expect(heading).toHaveTextContent('Github workflow');
   });
 });
